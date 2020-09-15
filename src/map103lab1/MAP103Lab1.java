27a746759d9e12bd7b6bef6b409f9301963bec2f
kranthi pyreddy <kra330@gmail.com>
2020-09-15 01:39:06 -0400

    Initial Commit

--- src/map103lab1/MAP103Lab1.java
+++ src/map103lab1/MAP103Lab1.java
@@ -16,6 +16,8 @@ public class MAP103Lab1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World");
+                
     }
     
 }
